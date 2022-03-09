04e95798c2eddef5a9fc3af6451e45ceab008d2b
oajh04 <cloudtubekr@naver.com>
2022-03-09 23:11:30 +0900

    ⚡️ :: app vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Home from './views/Home.vue'
+export default {
+  name: 'App',
+  components: { Home },
+}
+</script>
+<style src="./App.css" />
