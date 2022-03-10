6cf89473f5728822f1ee7397647bdcf4decad74e
oajh04 <cloudtubekr@naver.com>
2022-03-10 20:20:11 +0900

    ⚡️ :: router main 설정

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Main from '../views/Main.vue'
 import Register from '../views/Register.vue'
 
 const router = createRouter({
@@ -9,6 +10,7 @@ const router = createRouter({
     { path: '/', component: Home },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
+    { path: '/main', component: Main },
   ],
 })
 export default router
