1ed19b1b80ef5ade905560d788e948e27068cb93
oajh04 <cloudtubekr@naver.com>
2022-03-10 20:18:33 +0900

    ⚡️ :: Header

---
+++ src/components/common/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper">
+    <font-awesome-icon icon="trophy" class="fa-xl" />
+    <font-awesome-icon icon="bars" class="fa-xl" @click="IS_OPEN_MENU(true)" />
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+export default {
+  methods: {
+    ...mapMutations(['IS_OPEN_MENU']),
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+  height: 70px;
+  background-color: var(--header-color);
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  padding: 10px;
+}
+.wrapper svg {
+  margin: 10px;
+  cursor: pointer;
+}
+</style>
