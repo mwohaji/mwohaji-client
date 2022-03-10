3195c9e8374cfaa725ae0c7127e33345d7021e71
oajh04 <cloudtubekr@naver.com>
2022-03-10 20:19:32 +0900

    ⚡️ :: Main ui

---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header />
+  <Menu />
+  <!-- <word-card></word-card> -->
+</template>
+
+<script>
+import Header from '../components/common/Header.vue'
+import Menu from '../components/Menu.vue'
+import WordCard from '../components/WordCard.vue'
+export default {
+  components: { WordCard, Header, Menu },
+}
+</script>
+
+<style scoped></style>
