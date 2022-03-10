646212c41397e5ace4eb5716fc7a65731b4f9c3b
oajh04 <cloudtubekr@naver.com>
2022-03-10 20:20:46 +0900

    ⚡️ :: 테마 모드 확인

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,19 @@ import Home from './views/Home.vue'
 export default {
   name: 'App',
   components: { Home },
+  mounted() {
+    const isUserColorTheme = localStorage.getItem('color-theme')
+    if (isUserColorTheme === 'system' || isUserColorTheme === null) {
+      const isOsColorTheme = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches
+        ? 'dark'
+        : 'light'
+
+      document.documentElement.setAttribute('color-theme', isOsColorTheme)
+    } else {
+      document.documentElement.setAttribute('color-theme', isUserColorTheme)
+    }
+  },
 }
 </script>
 <style src="./App.css" />
