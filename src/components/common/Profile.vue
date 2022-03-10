973533182d3b9e425f80bed9ab3b59367d4495b1
oajh04 <cloudtubekr@naver.com>
2022-03-10 20:18:43 +0900

    ⚡️ :: profile image

---
+++ src/components/common/Profile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="profile-wrapper">
+    <img class="profile-img" alt="" />
+    <div>
+      <div class="name">한준호</div>
+      <div class="email">cloudtubekr@naver.com</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.profile-wrapper {
+  display: flex;
+  align-items: center;
+}
+.profile-img {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+.name {
+  font-size: 20px;
+  font-weight: bold;
+}
+.email {
+  font-size: 16px;
+  width: 180px;
+  display: block;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+</style>
