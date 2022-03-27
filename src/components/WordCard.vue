383e33f9fb2cb413442ab84a839c8f15e40617f4
oajh04 <cloudtubekr@naver.com>
2022-03-27 23:36:31 +0900

    ⚡️ :: wordcard

---
+++ src/components/WordCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <div class="text">수영하다</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.wrapper {
+  max-width: 400px;
+  width: 100%;
+  height: 168px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid var(--border-color);
+  border-radius: 10px;
+}
+.text {
+  font-size: 40px;
+  font-weight: bold;
+  color: var(--font-color);
+}
+</style>
